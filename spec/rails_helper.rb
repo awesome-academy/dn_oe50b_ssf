a22ad52548061a8d4655f928b374dbf762571743
nguyenvanson332000 <nguyenvanson332000@gmail.com>
2021-12-15 07:30:36 -0800

    rspec_controller_soccer_field

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,11 +2,11 @@ require "spec_helper"
 require "shoulda/matchers"
 require "support/database_cleaner"
 ENV["RAILS_ENV"] ||= "test"
-require "shoulda/matchers"
 require File.expand_path("../config/environment", __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
 require "support/factory_bot"
+require "support/auth_helper"
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
@@ -20,6 +20,8 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.include(AuthHelper, type: :request)
+  config.include Devise::Test::IntegrationHelpers, type: :request
 end
 
 Shoulda::Matchers.configure do |config|
